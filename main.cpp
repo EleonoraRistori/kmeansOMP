50756bab854bd0b98dbf834e954fae81fa513148
EleonoraRistori <eleristo@gmail.com>
2023-04-30 16:57:58 +0200

    Report added

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@
 
 
 int main() {
-    const int num_points=50000000;  // number of experiments
+    const int num_points=15000000;  // number of experiments
     const int data_point_dim = 2;
     int k = 5;
     const float sigma = 5;
@@ -23,30 +23,31 @@ int main() {
     std::vector<std::chrono::milliseconds> parallelDuration;
 
     int num_it = 0;
-    int num_threads = 12;
-//    //Test #threads
-//    points = new float[num_points*data_point_dim];
-//    cluster_assignment = new int[num_points];
-//    centroids = new float[k*data_point_dim];
-//    generateCluster(num_points, data_point_dim, k, sigma, points, centroids);
-//    //saveArray(num_points, data_point_dim, points, "points.csv");
-//    for(int num_threads = 1; num_threads <= 24; num_threads++){
-//        auto begin = std::chrono::high_resolution_clock::now();
-//        kmeans(points, centroids, cluster_assignment, num_points, data_point_dim, k, 20, sigma, num_threads);
-//        auto end = std::chrono::high_resolution_clock::now();
-//        parallelDuration.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin));
-//        std::cout << num_threads;
-//
-//    }
+
+    //Test #threads
+    points = new float[num_points*data_point_dim];
+    cluster_assignment = new int[num_points];
+    centroids = new float[k*data_point_dim];
+    generateCluster(num_points, data_point_dim, k, sigma, points, centroids);
+    //saveArray(num_points, data_point_dim, points, "points.csv");
+    for(int num_threads = 1; num_threads <= 24; num_threads++){
+        auto begin = std::chrono::high_resolution_clock::now();
+        kmeans(points, centroids, cluster_assignment, num_points, data_point_dim, k, 20, sigma, num_threads);
+        auto end = std::chrono::high_resolution_clock::now();
+        parallelDuration.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin));
+        std::cout << num_threads << "\n";
+        std::cout << parallelDuration[num_threads-1].count() << "\n";
+
+    }
 //    //saveArray(num_points, data_point_dim, cluster_assignment, "cluster_assignment.csv");
 //    delete[] points;
 //    delete[] cluster_assignment;
 //    delete[] centroids;
-//    saveTime(23, parallelDuration, "p_time_n_threads.csv");
+    saveTime(24, parallelDuration, "p_time_n_threads.csv");
 //    parallelDuration.clear();
 
 //    //Test #points
-//    int num_threads = 6;
+//    int num_threads = 12;
 //    centroids = new float[k*data_point_dim];
 //
 
@@ -78,31 +79,32 @@ int main() {
 
 
 
-    //Test #cluster
-    for(k = 10; k <= 20010; k=k+2000){
-        num_it++;
-        points = new float[num_points*data_point_dim];
-        cluster_assignment = new int[num_points];
-        centroids = new float[k*data_point_dim];
-        std::cout << k;
-        generateCluster(num_points, data_point_dim, k, sigma, points, centroids);
-        auto begin = std::chrono::high_resolution_clock::now();
-        kmeans_s(points, centroids, cluster_assignment, num_points, data_point_dim, k, 20, sigma);
-        auto end = std::chrono::high_resolution_clock::now();
-        sequentialDuration.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin));
-        begin = std::chrono::high_resolution_clock::now();
-        kmeans(points, centroids, cluster_assignment, num_points, data_point_dim, k, 20, sigma, num_threads);
-        end = std::chrono::high_resolution_clock::now();
-        parallelDuration.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin));
-        std::cout << sequentialDuration[num_it-1].count() << "\n";
-        std::cout << parallelDuration[num_it-1].count() << "\n";
-        delete[] points;
-        delete[] cluster_assignment;
-        delete[] centroids;
-
-    }
-    saveTime(num_it, sequentialDuration, "time_n_centroids.csv");
-    saveTime(num_it, parallelDuration, "p_time_n_centroids.csv");
+//    //Test #cluster
+    //int num_threads = 12;
+//    for(k = 10; k <= 20010; k=k+2000){
+//        num_it++;
+//        points = new float[num_points*data_point_dim];
+//        cluster_assignment = new int[num_points];
+//        centroids = new float[k*data_point_dim];
+//        std::cout << k << "\n";
+//        generateCluster(num_points, data_point_dim, k, sigma, points, centroids);
+//        auto begin = std::chrono::high_resolution_clock::now();
+//        kmeans_s(points, centroids, cluster_assignment, num_points, data_point_dim, k, 20, sigma);
+//        auto end = std::chrono::high_resolution_clock::now();
+//        sequentialDuration.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin));
+//        begin = std::chrono::high_resolution_clock::now();
+//        kmeans(points, centroids, cluster_assignment, num_points, data_point_dim, k, 20, sigma, num_threads);
+//        end = std::chrono::high_resolution_clock::now();
+//        parallelDuration.push_back(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin));
+//        std::cout << sequentialDuration[num_it-1].count() << "\n";
+//        std::cout << parallelDuration[num_it-1].count() << "\n";
+//        delete[] points;
+//        delete[] cluster_assignment;
+//        delete[] centroids;
+//
+//    }
+//    saveTime(num_it, sequentialDuration, "time_n_centroids.csv");
+//    saveTime(num_it, parallelDuration, "p_time_n_centroids.csv");
 
     return 0;
 }
